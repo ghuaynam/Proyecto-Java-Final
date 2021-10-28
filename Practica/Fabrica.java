029defd0e71855be2271b74c75ff0a8c4eb6de4c
ghuaynam <1400611@utp.edu.pe>
2021-10-28 18:25:32 -0500

    add project java

---
+++ Practica/Fabrica.java
@@ -0,0 +1,15 @@
+
+/**
+ * Clase principal del proyecto para la práctica de la asignatura Programación 
+ * Orientada a Objetos, para el curso 2019/2020
+ * 
+ * @author Arturo Barba Rodríguez abarba54@alumno.uned.es
+ * @version 1.0
+ */
+public class Fabrica
+{
+    public static void main(String[] args) {
+        Prueba.cargarDatos();
+        Menu.iniciar();
+    }
+}
